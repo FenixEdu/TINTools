09804e9d5ed1e6c3103d30582cd3d22c7404bff1
Luis Cruz <luis.cruz@tecnico.pt>
2018-12-07 11:45:29 +0000

    Custom result when there is no validator

--- src/main/java/org/fenixedu/TINValidator.java
+++ src/main/java/org/fenixedu/TINValidator.java
@@ -5,6 +5,10 @@ import eu.europa.ec.taxud.tin.algorithm.ITinAlgorithm;
 public class TINValidator {
 
     public static boolean isValid(final String countryCode, final String tin) {
+        return isValid(countryCode, tin, true);
+    }
+
+    public static boolean isValid(final String countryCode, final String tin, final boolean resultWhenThereIsNoValidator) {
         if (countryCode == null || countryCode.length() != 2) {
             return false;
         }
@@ -17,12 +21,11 @@ public class TINValidator {
             final ITinAlgorithm tinImplementation = (ITinAlgorithm) tinAlgo.newInstance();
             return tinImplementation.isValid(tin) == 0;
         } catch (ClassNotFoundException var4) {
-            return true;
+            return resultWhenThereIsNoValidator;
         } catch (IllegalAccessException var5) {
             return false;
         } catch (InstantiationException var6) {
             return false;
         }
     }
-
 }
